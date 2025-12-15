32ea060e9b3e93c67a1e3c76db64971c3829e8ad
Maicol <pirlo1121s@gmail.com>
2025-12-14 20:44:06 -0500

    port mailer

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -5,8 +5,13 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
+  port: "465", // puerto ( SSL )
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000, // 10 segundos
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
